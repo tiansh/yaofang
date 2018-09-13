358fdd5d8f2f976a947cc86ce1949fa062bb6737
田生 <tiansheng111@gmail.com>
2018-09-13 21:45:03 +1000

    一些兼容性修改

--- extension/background/request.js
+++ extension/background/request.js
@@ -14,8 +14,6 @@
   requests.forEach(({ id, filter }) => {
     browser.webRequest.onBeforeRequest.addListener(async details => {
       const { documentUrl, tabId } = details;
-      if (!documentUrl) return {};
-      if (!/^(?:.*\.)?weibo.com$/.test(new URL(documentUrl).hostname)) return false;
       return message.invoke(tabId).request(id, details).catch(error => ({}));
     }, filter, ['blocking']);
   });
--- extension/content/util/inject.js
+++ extension/content/util/inject.js
@@ -4,11 +4,25 @@
   const util = yawf.util = yawf.util || {};
 
   util.inject = function (func, ...args) {
-    const script = `void(${func}(${args.map(value => JSON.stringify(value))}));`;
-    return Promise.resolve().then(() => (
-      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts#Using_eval()_in_content_scripts
-      window.eval(script)  // eslint-disable-line
-    ));
+    const executeScript = `void(${func}(${args.map(value => JSON.stringify(value))}));`;
+    if (browser.runtime.getURL('/').startsWith('moz-extension:')) {
+      // https://bugzilla.mozilla.org/show_bug.cgi?id=1267027
+      return Promise.resolve().then(() => (
+        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts#Using_eval()_in_content_scripts
+        window.eval(executeScript)  // eslint-disable-line
+      ));
+    } else {
+      const script = document.createElement('script');
+      script.textContent = executeScript;
+      const target = document.head || document.body || document.documentElement || document.getElementsByTagName('*')[0];
+      return new Promise(resolve => {
+        script.addEventListener('load', () => {
+          resolve();
+          script.parentElement.removeChild(script);
+        });
+        target.appendChild(script);
+      });
+    }
   };
 
 }());
