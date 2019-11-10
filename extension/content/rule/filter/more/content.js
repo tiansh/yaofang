d6aea9f779bf7d963d75dfd1074b844e6b7a35f5
田生 <tiansheng111@gmail.com>
2019-11-10 17:13:25 +1100

    修改付费内容的识别方式

--- extension/content/rule/filter/more/content.js
+++ extension/content/rule/filter/more/content.js
@@ -374,8 +374,8 @@
         const paidOnly = +searchParams.get('vplus') || searchParams.get('is_vclub');
         if (paidOnly) return null;
         if (feed.querySelector('.icon_vplus')) return 'hide';
-        if (feed.querySelector('.WB_media_a[action-data*="isPrivate"]:not([action-data*="isPrivate=0"])')) return 'hide';
-        if (feed.querySelector('[action-type="fl_pics"][action-data*="isPrivate"]:not([action-data*="isPrivate=0"])')) return 'hide';
+        if (feed.querySelector('.WB_media_a[action-data*="isPrivate=1"]')) return 'hide';
+        if (feed.querySelector('[action-type="fl_pics"][action-data*="isPrivate=1"]')) return 'hide';
         return null;
       });
       this.addConfigListener(() => { observer.feed.rerun(); });
