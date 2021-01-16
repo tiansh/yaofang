099d6199ae8a3a09291063fd0de2a1036051b329
田生 <tiansheng111@gmail.com>
2021-01-16 22:36:02 +1100

    89: 新的跳转中间界面

--- extension/content/shorturl/redirect.js
+++ extension/content/shorturl/redirect.js
@@ -17,32 +17,38 @@ html { background: #f9f9fa; }
 `;
   document.documentElement.appendChild(hideAll);
 
+  const fixUrl = function (url) {
+    // 显示的字符编码是错的
+    // 原本 UTF-8 编码的网址用 Latin-1 展示的
+    let fixEncodingUrl = url;
+    try {
+      const codePoints = [...url].map(x => x.charCodeAt());
+      if (codePoints.every(code => code < 256)) {
+        fixEncodingUrl = new TextDecoder().decode(new Uint8Array(codePoints));
+      }
+    } catch (e) {
+      fixEncodingUrl = url;
+    }
+    if (!/https?:\/\/.*/i.test(fixEncodingUrl)) return null;
+    return fixEncodingUrl;
+  };
+
   const onLoad = function () {
     configPromise.then(() => {
       const useRedirect = config.global.key('short_url_wo_confirm').get();
       if (!useRedirect) return false;
       let url = [
+        () => document.querySelector('.open-url a').href,
         () => document.querySelector('.link').textContent.trim(),
         () => document.querySelector('.url_view_code').textContent.trim(),
       ].reduce((url, getter) => {
         if (url) return url;
         try {
-          return getter();
+          return fixUrl(getter());
         } catch (e) { return null; }
       }, null);
-      // 显示的字符编码是错的
-      // 原本 UTF-8 编码的网址用 Latin-1 展示的
-      let fixEncodingUrl = null;
-      try {
-        const codePoints = [...url].map(x => x.charCodeAt());
-        if (codePoints.every(code => code < 256)) {
-          fixEncodingUrl = new TextDecoder().decode(new Uint8Array(codePoints));
-        }
-      } catch (e) {
-        fixEncodingUrl = url;
-      }
-      if (!/https?:\/\/.*/i.test(fixEncodingUrl)) return false;
-      location.replace(fixEncodingUrl);
+      if (!url) return false;
+      location.replace(url);
       return true;
     }).then(r => r, () => false).then(redirect => {
       if (!redirect) hideAll.remove();
