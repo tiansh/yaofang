56869ffb3db14a319dd5649dd15caf611e60af30
田生 <tiansheng111@gmail.com>
2021-11-24 22:17:31 +1100

    change es version

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   root: true,
   extends: 'eslint:recommended',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     ecmaFeatures: {
       impliedStrict: true,
     },
